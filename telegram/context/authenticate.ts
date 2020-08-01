1aeb0d49d04a1254fba955b5587ec719640e938d
Rogério Munhoz <roz@rjmunhoz.me>
2020-08-01 16:08:53 +0000

    Add better error handling

--- telegram/context/authenticate.ts
+++ telegram/context/authenticate.ts
@@ -1,9 +1,15 @@
 import { NowContext } from '..'
+import { InvalidTokenError } from '../errors/InvalidTokenError'
 
 export async function authenticate (token: string, { req, res }: NowContext) {
+
+  if (!token) {
+    throw new Error('No token present')
+  }
+
   if (token !== req.query.token) {
     res.status(401).end()
-    throw new Error('no auth token')
+    throw new InvalidTokenError()
   }
 
   return { req, res }
